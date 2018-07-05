65c8a4414d8a4e5d0b60524da0cbfeabd4ca1a0e
Juan Carlos Merlos Albarracín <jc_merlos_servext_rsi@cajarural.com>
2018-07-05 13:36:42 +0200

    Fox IBOutlet's pageControl & ScrollView. OK

--- LandscapeViewController.swift
+++ LandscapeViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class LandscapeViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     // Para comprobar por consola que el objeto se
     // cancela correcta/ cuando la pantalla se cierra.
     deinit {
@@ -21,7 +24,46 @@ class LandscapeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        
+        // Como vamos a hacer nuestras propias restricciones y no las automáticas
+        // que nos pone el Interface Builder (IB) eliminamos las constrainsts
+        // actuales en la view, en pageControl y en el scrollView. La segunda
+        // instrucción nos permite colocar y modificar el tamaño de las vistas
+        // manualmente al poner dicha propiedad a true, con esto no entramos en
+        // conflicto con Auto Layout ya que para este controlador están activadas.
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+        
+    }
+    
+    
+    // El método "viewWillLayoutSubviews()"  es llamado por UIKit
+    // como parte de la fase de diseño de su controlador de vista
+    // cuando aparece por primera vez en la pantalla. Es el lugar
+    // ideal para cambiar el marco de sus puntos de vista a mano.
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        // Nota: Recuerde que los "bounds"" describen el rectángulo que conforma el interior de una vista,
+        // mientras que los "frames" el rectángulo que conforma el interior de una vista, mientras que
+        // el describir el rectángulo que conforma el interior de una vista. El "frame" del "scrollView"
+        // es el rectángulo visto desde la perspectiva de la vista principal, mientras que el "bounds"
+        // del "scrollVieww"es el mismo rectángulo del propio "scrollView".
+        // Debido a que el "scrollView" y el "page control" son los dos hijos de la vista principal,
+        // sus "frames" se sientan en la misma "coordinate space" como los "boounds" de la vista principal.
+        
+        
+        // La vista de desplazamienteo siempre debe ser tan grande como toda la pantalla,
+        // por lo que haremos su "frame" igual a los límites de la vista principal.
+        scrollView.frame = view.bounds
+        
+        pageControl.frame = CGRect(x: 0, y: view.frame.size.height - pageControl.frame.size.height, width: view.frame.size.width, height: pageControl.frame.size.height)
     }
     
     override func didReceiveMemoryWarning() {
